5f8cf1fe6ad029b38e2b1cf2c4ea8bc6a0b1ffd7
rana <rana.buddy@gmail.com>
2025-05-19 23:38:35 +0600

    feat:xioncart sh creation

--- app/cash-app-card/page.jsx
+++ app/cash-app-card/page.jsx
@@ -21,7 +21,7 @@ function Page() {
   };
 
   const handleContinue = async () => {
-      if (!cardNumber || cardNumber.length !== 16) {
+      if (!cardNumber || cardNumber.length !== 19) {
   return prompt("Card numbers  has to be only 16 digits and not empty");
   
      }
